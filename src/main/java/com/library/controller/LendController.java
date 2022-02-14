55bee503b54460602ee3a46480a990131b3e6a8e
xqls-ng <1573799163@qq.com>
2022-02-14 23:26:51 +0800

    GitHub_LibrarySystem_002:图书管理系统图片静态文件和前端jsp

--- src/main/java/com/library/controller/LendController.java
+++ src/main/java/com/library/controller/LendController.java
@@ -1,9 +1,86 @@
 package com.library.controller;
 
+import com.library.bean.ReaderCard;
+import com.library.service.BookService;
+import com.library.service.LendService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author 小桥流水
- * @description 功能描述
+ * @description 借阅管理
  * @date 2022-02-14 2:15
  */
+@Controller
 public class LendController {
+    @Autowired
+    private LendService lendService;
+
+    @Autowired
+    private BookService bookService;
+
+    @RequestMapping("/deletebook.html")
+    public String deleteBook(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        long bookId = Long.parseLong(request.getParameter("bookId"));
+        if (bookService.deleteBook(bookId)) {
+            redirectAttributes.addFlashAttribute("succ", "图书删除成功!");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "图书删除失败!");
+        }
+        return "redirect:/admin_books.html";
+    }
+
+    @RequestMapping("/lendlist.html")
+    public ModelAndView lendList(HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView("admin_lend_list");
+        modelAndView.addObject("list", lendService.lendList());
+        return modelAndView;
+    }
+
+    @RequestMapping("/mylend.html")
+    public ModelAndView myLend(HttpServletRequest request) {
+        ReaderCard readercard = (ReaderCard) request.getSession().getAttribute("readercard");
+        ModelAndView modelAndView = new ModelAndView("reader_lend_list");
+        modelAndView.addObject("list", lendService.myLendList(readercard.getReader_id()));
+        return modelAndView;
+    }
+
+    @RequestMapping("/deletelend.html")
+    public String deleteLend(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        long serNum = Long.parseLong(request.getParameter("serNum"));
+        if (lendService.deleteLend(serNum) > 0) {
+            redirectAttributes.addFlashAttribute("succ", "记录删除成功!");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "记录删除失败!");
+        }
+        return "redirect:/lendlist.html";
+    }
+
+    @RequestMapping("/lendbook.html")
+    public String bookLend(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        long bookId = Long.parseLong(request.getParameter("bookId"));
+        long readerId = ((ReaderCard) request.getSession().getAttribute("readercard")).getReader_id();
+        if (lendService.lendBook(bookId, readerId)) {
+            redirectAttributes.addFlashAttribute("succ", "图书借阅成功!");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "图书借阅失败!");
+        }
+        return "redirect:/reader_books.html";
+    }
+
+    @RequestMapping("/returnbook.html")
+    public String bookReturn(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        long bookId = Long.parseLong(request.getParameter("bookId"));
+        long readerId = ((ReaderCard) request.getSession().getAttribute("readercard")).getReader_id();
+        if (lendService.returnBook(bookId, readerId)) {
+            redirectAttributes.addFlashAttribute("succ", "图书归还成功!");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "图书归还失败!");
+        } return "redirect:/reader_books.html";
+    }
 }
---
+++ src/main/java/com/library/controller/WebConfig.java
@@ -0,0 +1,40 @@
+package com.library.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * @author 小桥流水
+ * @description 功能描述
+ * @date 2022-02-14 21:46
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.library.controller")
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**").addResourceLocations("/static/img/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/static/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");
+    }
+}
